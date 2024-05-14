2a0d12ba3e42c4c5861cb5ad911f4e4eab73e889
ThomasPierli <thomaspierli@gmail.com>
2024-05-14 15:16:33 +0200

    Delete gestionale directory

--- gestionale/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module gest {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.graphics;
-    requires java.desktop;
-
-    opens gest to javafx.fxml;
-    exports gest;
-}
