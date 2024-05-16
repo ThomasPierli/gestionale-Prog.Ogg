83aaa6a0d485b050eae8fe8908dfdc28e17b9dff
ThomasPierli <thomaspierli@gmail.com>
2024-05-16 10:39:22 +0200

    Update Supplier.java

--- src/main/java/gest/logic/Supplier.java
+++ src/main/java/gest/logic/Supplier.java
@@ -1,16 +1,29 @@
 package gest.logic;
 
 import java.util.ArrayList;
+
 import gest.logic.principalclasscomponent.Order;
 import gest.logic.principalclasscomponent.Person;
 
 public class Supplier extends Person{
-    
+
+
+    private String Info;    
     private ArrayList<Order> Orders;
 
-    public Supplier(String firstName, String lastName, String email, String phoneNumber, ArrayList<Order> orders) {
+    public Supplier(String firstName, String lastName, String email, String phoneNumber, String info,
+            ArrayList<Order> orders) {
         super(firstName, lastName, email, phoneNumber);
+        Info = info;
         Orders = orders;
+    }
+
+    public String getInfo() {
+        return Info;
+    }
+
+    public void setInfo(String info) {
+        Info = info;
     }
 
     public ArrayList<Order> getOrders() {
@@ -19,8 +32,6 @@ public class Supplier extends Person{
 
     public void setOrders(ArrayList<Order> orders) {
         Orders = orders;
-    }
-
-    
+    }   
 
 }
